56cd906506dc5d0c2d45868acd97770c24a7d27c
Sockmonkey11 <goodcupcakes344@gmail.com>
2025-08-14 23:00:06 -0400

    Change

--- niche_bot.py
+++ niche_bot.py
@@ -3,10 +3,6 @@ import config
 import schedule
 import requests
 import time
-import threading
-from flask import Flask
-
-
 DISCHORD_WEBHOOK_URL = config.DISCHORD_WEBHOOK_URL
 def bot_login():
     print ("Logging in...")
@@ -30,27 +26,14 @@ def run_bot(r):
         list_number += 1
         print(f"{list_number}. {title}")
         
-app = Flask('')
 
-@app.route('/')
-def home():
-    return "Bot is alive!"
-
-def run_flask():
-    app.run(host='0.0.0.0', port=8080)
-    
-threading.Thread(target=run_flask).start()
-
-    
 r = bot_login()
+run_bot(r)
+# schedule.every().day.at("10:35").do(run_bot, r)
 
-# Schedule the bot to run every day at 10:35 AM
-schedule.every().day.at("10:35").do(run_bot, r)
-
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
 # This code logs into Reddit, retrieves the 10 hottest posts from r/wallstreetbets, and sends their titles to a Discord webhook.
 
         
